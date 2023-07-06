0f19f09b2043c656809aefe2ed9e385e0ed23ec9
david setyawan <45872505+davidsetyawan@users.noreply.github.com>
2023-07-06 13:52:51 +0700

    dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,15 @@
 // Navbar fixed
-window.onscroll = function(){
+window.onscroll = function () {
 	const header = document.querySelector('header');
 	const fixedNax = header.offsetTop;
+	const toTop = document.querySelector('#to-top')
 
-	if(window.pageYOffset > fixedNax){
+	if (window.scrollY > fixedNax) {
 		header.classList.add('navbar-fixed');
-	}else{
+		toTop.classList.remove('hidden');
+	} else {
 		header.classList.remove('navbar-fixed');
+		toTop.classList.add('hidden');
 	}
 }
 
@@ -14,7 +17,36 @@ window.onscroll = function(){
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener('click', function(){
+hamburger.addEventListener('click', function () {
 	hamburger.classList.toggle('hamburger-active');
 	navMenu.classList.toggle('hidden')
 })
+
+// Klik di luar hamburger
+window.addEventListener('click', function (e) {
+	if (e.target != hamburger && e.target != navMenu) {
+		hamburger.classList.remove('hamburger-active');
+		navMenu.classList.add('hidden')
+	}
+})
+
+// Dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html")
+
+darkToggle.addEventListener('click', function () {
+	if (darkToggle.checked) {
+		html.classList.add('dark');
+		localStorage.theme = 'dark'
+	} else {
+		html.classList.remove('dark')
+		localStorage.theme = 'light'
+	}
+})
+
+// pindah posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+	darkToggle.checked = true
+} else {
+	darkToggle.checked = false
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
-  theme: {
-	container:{
-		center:true,
-		padding: '1rem'
-	},
-    extend: {
-		colors:{
-			'primary': '#14b8a6',
-			'dark' : '#0f172a',
-			'secondary': '#64748b',
+	content: ['index.html'],
+	darkMode: 'class',
+	theme: {
+		container: {
+			center: true,
+			padding: '1rem'
 		},
-		screens:{
-			'xl' : '1320px'
-		}
+		extend: {
+			colors: {
+				'primary': '#14b8a6',
+				'dark': '#0f172a',
+				'secondary': '#64748b',
+			},
+			screens: {
+				'xl': '1320px'
+			}
+		},
 	},
-  },
-  plugins: [],
+	plugins: [],
 }
 
